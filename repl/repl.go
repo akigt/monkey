5b82251a219b591bf0fc637c3a20f4127282142c
Aki <mifukia1101@gmail.com>
2018-07-27 19:14:29 +0900

    191 done

--- repl/repl.go
+++ repl/repl.go
@@ -24,6 +24,11 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
+
+		if line == "exit" {
+			io.WriteString(out, "See you!\n")
+			return
+		}
 		l := lexer.New(line)
 		p := parser.New(l)
 
